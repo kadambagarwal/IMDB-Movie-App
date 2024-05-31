056226c66fcc6499277ffaa3b647ab42feeb0783
Kadamb Agarwal <kadambagarwal3@gmail.com>
2024-05-31 17:57:51 +0530

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by kadagarw on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
