586d809e20b8617faebf0bbd9bc60c3e30102537
Kadamb Agarwal <kadambagarwal3@gmail.com>
2024-06-01 14:25:34 +0530

    Add the functionality of toggle in trending

--- MovieApp/ContentView.swift
+++ MovieApp/ContentView.swift
@@ -10,27 +10,25 @@ import Kingfisher
 
 struct MovieView: View {
     @State private var trendingMovies = [Movie]()
-    // Add more state variables for the other sections as needed
     private let imageBaseUrl = "https://image.tmdb.org/t/p/w500"
+    private let movieDatabase = MovieDatabase()
     
     var body: some View {
         ScrollView {
             VStack {
-                MovieSectionView(title: "Trending", movies: trendingMovies)
-                // Add more MovieSectionViews for the other sections as needed
+                MovieSectionView(title: "Trending", movies: $trendingMovies, loadMovies: loadTrendingMovies)
             }
         }
-        .onAppear(perform: loadTrendingMovies)
+        .onAppear(perform: { loadTrendingMovies(timePeriod: "Day") })
     }
     
-    func loadTrendingMovies() {
-        let movieDatabase = MovieDatabase()
-        movieDatabase.fetchMovies(endpoint: .trendingToday) { result in
+    func loadTrendingMovies(timePeriod: String) {
+        let endpoint: MovieEndpoint = timePeriod == "Day" ? .trendingToday : .trendingThisWeek
+        movieDatabase.fetchMovies(endpoint: endpoint) { result in
             switch result {
             case .success(let movieResponse):
                 DispatchQueue.main.async {
                     self.trendingMovies = movieResponse.results
-                    // Load movies for the other sections as needed
                 }
             case .failure(let error):
                 print("Error: \(error)")
@@ -41,12 +39,14 @@ struct MovieView: View {
 
 struct MovieSectionView: View {
     var title: String
-    var movies: [Movie]
+    @Binding var movies: [Movie]
     @State private var timePeriod = "Day"
+    var loadMovies: (String) -> Void
     
-    init(title: String, movies: [Movie]) {
+    init(title: String, movies: Binding<[Movie]>, loadMovies: @escaping (String) -> Void) {
         self.title = title
-        self.movies = movies
+        self._movies = movies
+        self.loadMovies = loadMovies
         UISegmentedControl.appearance().selectedSegmentTintColor = UIColor.systemTeal
         UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
         UISegmentedControl.appearance().backgroundColor = UIColor.white
@@ -65,7 +65,8 @@ struct MovieSectionView: View {
                     Text("This week").tag("Week")
                 }
                 .pickerStyle(SegmentedPickerStyle())
-                .frame(width: 150) // Increase the width here
+                .frame(width: 150)
+                .onChange(of: timePeriod, perform: loadMovies)
             }
             .padding(.horizontal)
             
